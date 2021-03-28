c47a01d964e580bd3319cded0353f874fb9b14f5
Peter Schmalfeldt <peter@manifestinteractive.com>
2021-03-28 01:06:14 -0400

    Fixing some more bugs

--- resources/views/components/partial/approach-to-policing/chart-arrests-by-year.blade.php
+++ resources/views/components/partial/approach-to-policing/chart-arrests-by-year.blade.php
@@ -1,9 +1,9 @@
 @if (isset($scorecard['arrests']['arrests_2016']) && isset($scorecard['arrests']['arrests_2017']) && isset($scorecard['arrests']['arrests_2018']))
 <div class="stat-wrapper">
     <h3>Arrests By Year</h3>
+    <p>{{ num($scorecard['report']['total_arrests']) }} Arrests Reported from 2013-19</p>
     <p style="margin-top: 18px; margin-bottom: 6px;">
         <canvas id="bar-chart-arrests"></canvas>
     </p>
-    <p class="note">^&nbsp; {{ num($scorecard['report']['total_arrests']) }} Arrests Reported from 2013-19</p>
 </div>
 @endif
